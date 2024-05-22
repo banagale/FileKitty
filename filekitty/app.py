6e3f3789da6bd9c9a522cf23067199cd2db4c091
banagale <robert@weave.bio>
2024-05-22 14:44:48 -0700

    Clean up imports and apply some formatting

--- filekitty/app.py
+++ filekitty/app.py
@@ -1,12 +1,12 @@
 import os
 
-from PyQt5.QtCore import QSettings
-from PyQt5.QtCore import QTimer
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QSettings, QTimer, Qt
 from PyQt5.QtGui import QIcon, QGuiApplication, QKeySequence, QDragEnterEvent, QDropEvent
-from PyQt5.QtWidgets import (QApplication, QWidget, QFileDialog, QVBoxLayout, QPushButton, QTextEdit,
-                             QLabel, QListWidget, QDialog, QLineEdit, QHBoxLayout, QAction, QMenuBar)
-from PyQt5.QtWidgets import QGraphicsColorizeEffect
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QFileDialog, QVBoxLayout, QPushButton, QTextEdit,
+    QLabel, QListWidget, QDialog, QLineEdit, QHBoxLayout, QAction, QMenuBar,
+    QGraphicsColorizeEffect
+)
 
 ICON_PATH = 'assets/icon/FileKitty-icon.png'
 
@@ -121,8 +121,10 @@ class FilePicker(QWidget):
     def openFiles(self):
         default_path = self.get_default_path() or ""
         options = QFileDialog.Options()
-        files, _ = QFileDialog.getOpenFileNames(self, "Select files to concatenate", default_path,
-                                                "All Files (*);;Text Files (*.txt)", options=options)
+        files, _ = QFileDialog.getOpenFileNames(
+            self, "Select files to concatenate", default_path,
+            "All Files (*);;Text Files (*.txt)", options=options
+        )
         if files:
             self.fileList.clear()
             self.currentFiles = files
@@ -139,8 +141,8 @@ class FilePicker(QWidget):
             relative_path = os.path.relpath(file, start=common_prefix)
             self.fileList.addItem(relative_path)
             concatenated_content += f"### `{relative_path}`\n\n```\n"
-            with open(file, 'r', encoding='utf-8') as file:
-                content = file.read()
+            with open(file, 'r', encoding='utf-8') as f:
+                content = f.read()
                 concatenated_content += content
             concatenated_content += "\n```\n\n"
         return concatenated_content.rstrip()
