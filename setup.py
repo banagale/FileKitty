15803689adf6ab004531112df42bc36d33481419
banagale <rob@banagale.com>
2025-05-01 08:03:58 -0700

    Allow drag and drop of files onto command-tab icon or launched or unlaunched application icon in dock

--- setup.py
+++ setup.py
@@ -6,6 +6,30 @@ OPTIONS = {
     "argv_emulation": False,
     "packages": ["PyQt5"],
     "iconfile": "assets/icon/FileKitty-icon.icns",
+    "plist": {
+        "CFBundleIdentifier": "com.banagale.filekitty",
+        "CFBundleName": "FileKitty",
+        "CFBundleDisplayName": "FileKitty",
+        "CFBundleShortVersionString": "0.2.0",
+        "CFBundleVersion": "0.2.0",
+        "CFBundlePackageType": "APPL",
+        "NSHighResolutionCapable": True,
+        "CFBundleIconFile": "FileKitty-icon.icns",
+        "CFBundleDocumentTypes": [
+            {
+                "CFBundleTypeName": "All Files",
+                "CFBundleTypeRole": "Editor",
+                "LSHandlerRank": "Alternate",
+                "LSItemContentTypes": ["public.data"],
+                "CFBundleTypeIconFile": "FileKitty-icon.icns",
+            }
+        ],
+    },
+    "includes": ["sip", "PyQt5", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets"],
+    "resources": [
+        "/Users/rob/Library/Caches/pypoetry/virtualenvs/filekitty-YKgFKg7N-py3.12/lib/python3.12/site-packages/PyQt5/Qt5/plugins"
+    ],
+    "excludes": ["test", "tests", "unittest", "tkinter", "doctest"],
 }
 
 setup(
